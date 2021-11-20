ee70c53e1e35eb8f87288dd8e0c9ba172617bf57
Hodan <you@example.com>
2021-11-20 11:09:51 -0500

    finished generate btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,102 @@
-// VARIABLES
-var passwordText = document.querySelector("#password");
-var numb = "0,1,2,3,4,5,6,7,8,9";
-var lowCas = "abcdefghijklmnopqrstuvwxyz";
-var uppCas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var speChar = "!@#$%^&*()";
-var password = (" ");
-  var allValue = (" "); 
+//This is a random passowrd generator with prompts for criterias and a charater limit between 8-128
+
+// // VARIABLES
+// var passwordText = document.querySelector("#password");
+var generateBtn = document.querySelector("#generate");
+// var numb = "0,1,2,3,4,5,6,7,8,9";
+// var lowCas = "abcdefghijklmnopqrstuvwxyz";
+// var uppCas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// var speChar = "!@#$%^&*()";
+var passLength = "";
+
 // Assignment code here
 
-debugger;
+//debugger;
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// Our function to create all the criteria for password
+function generatePassword() {
+  // Prompt for password legnth
 
+  passLength = window.prompt(
+    "How long would you like your password? (Please Pick a number between 8 and 128)"
+  );
 
-function generatePassword(){
- 
-
-  var passwordStarter = window.alert("You will be given a series of prompts to customize your password.");
-
-  var passLength = window.prompt( "How long would you like your password?", "Pick a number between 8 and 128");
-
-
-  if ( passLength >= 8 && isNaN(passLength)===true && passLength <= 128) {
-    alert("Password must be at least 8 characters but no greater than 128 characters.");
-    passLength = window.prompt( "How long would you like your password?", "Pick a number between 8 and 128");
+  var lengthOf = parseInt(passLength);
+  console.log(lengthOf);
+  if (Number.isNaN(passLength)){
+    alert("Password length must be a number")
+    return null;
   }
+
+  if (lengthOf < 8 || lengthOf > 128) {
+    alert("Password must be greater than 8 but less than 128");
+    return null;
   
-  // var numbers = window.confirm("Do you want to include numberical values?");
-  // var upCase = window.confirm( "Do you want to include uppercase letters?");
-  // var lowCase = window.confirm("Do you want to include lowercase letters?");
-  // var special = window.confirm("DO you want to include special characters?");
-};
+  }  
+  
+  
+    var numbers = window.confirm(
+      "Click okay if you want to include numberical values?"
+    );
+   
+  
+  
+    var upCase = window.confirm(
+      "Click okay if you want to include uppercase letters?"
+    );
+  
+
+
+  
+    var lowCase = window.confirm(
+    "Click okay if you want to include lowercase letters?"
+  );
+    
+
+    var special = window.confirm(
+    "Click okay if you want to include special characters?"
+  );
+    
+  if ( numbers == false && upCase== false && lowCase== false && special== false) {
+    alert( "You must select at least one criteria.");
+    return null;
+  }
+
+  var userSelections = {
+    numbers: numbers,
+    lowCase: lowCase,
+    upCase: upCase,
+    special: special
+  };
+
+}
+
+generateBtn.addEventListener("mouseup", generatePassword);
+
+//Prompts for parameters of the password
 
 
 
+// const newLocal = false;
+// // Making sure one parameter was selected
 
-// // Write password to the #password input
+
+
+  var randomPassword = "";
+  for (var x=0; x < passLength; x++) {
+    randomPassword =  randomPassword =userSelections[Math.floor(Math.random() * userSelections.length)]
+  }
+
+
+// Write password to the #password input
+/*
 function writePassword() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+}
+*/
 
-
-// }
-
-function generatePassword ()
-
-/Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//  //The event listener for the click
+// generateBtn.addEventListener("click", writePassword);
